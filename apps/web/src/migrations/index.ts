0ba46ee6322eb0f0356e70462c85759e5ffef183
Sebastián Maluk <samaluk@miuandes.cl>
2026-09-02 12:57:43 -0400

    refactor: squash schema into baseline migration

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,15 +1,9 @@
 import * as migration_20260604_000000_baseline from './20260604_000000_baseline'
-import * as migration_20260902_000000_rename_active_memberships_to_memberships from './20260902_000000_rename_active_memberships_to_memberships'
 
 export const migrations = [
   {
     down: migration_20260604_000000_baseline.down,
     name: '20260604_000000_baseline',
     up: migration_20260604_000000_baseline.up,
-  },
-  {
-    down: migration_20260902_000000_rename_active_memberships_to_memberships.down,
-    name: '20260902_000000_rename_active_memberships_to_memberships',
-    up: migration_20260902_000000_rename_active_memberships_to_memberships.up,
   },
 ]
